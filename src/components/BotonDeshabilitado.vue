05cf993be0ec813b7c0cfb908c4716a2006a9325
C-RB <134893101+C-RB@users.noreply.github.com>
2024-05-17 16:14:29 -0400

    tutorial2
    Se realizaron los distintos componentes del tutorial

---
+++ src/components/BotonDeshabilitado.vue
@@ -0,0 +1,13 @@
+<template>
+    <button v-bind:disabled="botonDeshabilitado">Haz clic en mí</button>
+    </template>
+    <script>
+    export default {
+    data() {
+    return {
+    botonDeshabilitado: true
+    }
+    }
+    }
+    </script>
+    
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+    <MostrarMensaje/>
+    <DirectivaShowComponent/>
+    <DirectivaFor/>
+    <BotonDeshabilitado/>
+    <MensajeComponent/>
+    <ContadorComponent/>
+    <PadreComponent/>
+ 
+</template>
+
+<script>
+// @ is an alias to /src
+
+import MostrarMensaje from '@/components/MostrarMensaje.vue';
+import DirectivaFor from'@/components/DirectivaFor.vue';
+import DirectivaShowComponent from'@/components/DirectivaShowComponent.vue';
+import BotonDeshabilitado from'@/components/BotonDeshabilitado.vue';
+import MensajeComponent from'@/components/MensajeComponent.vue';
+import ContadorComponent from '@/components/ContadorComponent.vue';
+import PadreComponent from '@/components/PadreComponent.vue';
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    MostrarMensaje,
+    DirectivaShowComponent,
+    DirectivaFor,
+    BotonDeshabilitado,
+    MensajeComponent,
+    ContadorComponent,
+    PadreComponent
+
+  }
+}
+</script>
